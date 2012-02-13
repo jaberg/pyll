12f8fafe7a4b6fac87000e12e20597b06634deae
James Bergstra <james.bergstra@gmail.com>
2012-02-13 09:52:43 -0500

    m

--- pyll/__init__.py
+++ pyll/__init__.py
@@ -1,7 +1,9 @@
-from annotate import vectorize
 from base import as_apply
 from base import scope
 from base import rec_eval
 from base import clone
 from base import dfs
+
+# -- adds to scope
+import stochastic
 
